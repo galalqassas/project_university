e81b4b032f932dcc69fc91f8f3ac27fd3420ea61
LENOVO <s-ibrahem.ali@zewailcity.edu.eg>
2023-12-17 17:19:06 +0200

    KING by KING

--- main.cpp
+++ main.cpp
@@ -4,4 +4,5 @@ using namespace std;
 
 int main() {
     // hey ibrahim please learn github
+    // The is the king not the king but the king is not kinging you know what i mean right, let me tell you in the next message
 }
